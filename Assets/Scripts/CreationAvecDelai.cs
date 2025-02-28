a10a6e4a177bcac8eb295655cc8dcc39adafdfc5
GabbDup <gabriel.dupras01@gmail.com>
2025-02-28 11:45:56 -0500

    exercices augmentation de temps

--- Assets/Scripts/CreationAvecDelai.cs
+++ Assets/Scripts/CreationAvecDelai.cs
@@ -7,18 +7,28 @@ public class CreationAvecDelai : MonoBehaviour
     [SerializeField ] private GameObject _objetACreer;
     [SerializeField] private GameObject _objetPlacemenent;
 
+    private int _countCoins;
+
     void Start()
     {
+
+        _countCoins = 0;
         // Éxecute la méthode CreerObjet après un délai de 5s
-        Invoke("CreerObjet", 5f);
+        //Invoke("CreerObjet", 5f);
         // Éxecute la méthode CreerObjet après un délai initiale de 3s et à chaque 1s après
         InvokeRepeating("CreerObjet", 3f, 1f);
         // Cancelle tous les Invoke() actifs
-        CancelInvoke();
+        //CancelInvoke();
     }
 
     void CreerObjet()
     {
+
+        if (_countCoins == 10)
+        {
+            CancelInvoke();
+        }
+
         // Instancie un nouveau objet et garde une référence à lui
         GameObject nouvelleCopie = Instantiate(_objetACreer, _objetPlacemenent.transform.position, _objetPlacemenent.transform.rotation);
 
@@ -30,5 +40,7 @@ public class CreationAvecDelai : MonoBehaviour
 
         // Applique une force de rotation initiale aléatoire
         _rbNouvelleCopie.AddRelativeTorque(0f, Random.value, Random.value, ForceMode.Impulse);
+
+        _countCoins++;
     }
 }
--- Assets/Scripts/JeuV2.cs
+++ Assets/Scripts/JeuV2.cs
@@ -6,12 +6,25 @@ using UnityEngine;
 public class JeuV2 : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _etiquettePoints;
+    [SerializeField] private TextMeshProUGUI _etiquetteTemps;
 
     public int pointsJeu = 0;
+
+    public float tempsJeu = 0f;
+
+    private int time;
 
     void Start()
     {
         pointsJeu = 0;
+        tempsJeu = 0f;
+        InvokeRepeating("AugmenterTemps", 1f, 0.01f);
+    }
+
+    public void AugmenterTemps()
+    {
+        tempsJeu+= 0.01f;
+        _etiquetteTemps.text = tempsJeu.ToString("00.00") + "s";
     }
 
     public void AugmenterPoints()
